cafb5dc92a8e3a5ac0b40739f63743c1bb1a8251
uciel quiroga ortiz <ucielagustinquiroga@gmail.com>
2021-08-10 21:14:18 -0300

    Mas config

---
+++ ArrendaSys/Controllers/Api/CuentaController.cs
@@ -0,0 +1,21 @@
+﻿using ArrendaSysServicios.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ArrendaSys.Controllers.Api
+{
+    public class CuentaController : ApiController
+    {
+        [System.Web.Http.ActionName("hola")]
+        [System.Web.Http.HttpGet]
+        public CuentaViewModel hola()
+        {
+            ArrendaSysServicios.ServicioCuenta _servicio = new ArrendaSysServicios.ServicioCuenta();
+            var tmp = _servicio.hola();
+            return tmp;
+        }
+    }
+}
---
+++ ArrendaSysServicios/ServicioCuenta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Configuration;
+using ArrendaSysModelos;
+using ArrendaSysServicios.Modelos;
+
+namespace ArrendaSysServicios
+{
+    public class ServicioCuenta : IDisposable
+    {
+        private ARRENDASYSEntities db = new ARRENDASYSEntities();
+
+        public CuentaViewModel hola()
+        {
+            CuentaViewModel cuenta = new CuentaViewModel();
+            var c = db.Cuenta.FirstOrDefault();
+            cuenta.email = c.emailCuenta;
+            return cuenta;
+        }
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
