df49dcea812e8cba8873a0fe80c18523338eb367
MauricioC13 <maury_13@hotmail.com.ar>
2021-09-07 19:56:51 -0300

    Notificaciones

---
+++ ArrendaSys/Controllers/NotificacionesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+//using ArrendaSysModelos;
+
+
+namespace ArrendaSys.Controllers
+{
+    public class NotificacionesController : Controller
+    {
+        // GET: Notificaciones
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
