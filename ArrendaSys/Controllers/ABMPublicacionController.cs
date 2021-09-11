7503c6e649ec36fffc7ff8859c1c8e415358e5ae
MauricioC13 <maury_13@hotmail.com.ar>
2021-09-11 15:34:21 -0300

    ABMPublicacion

---
+++ ArrendaSys/Controllers/ABMPublicacionController.cs
@@ -0,0 +1,18 @@
+﻿using ArrendaSys.Controllers.Acceso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArrendaSys.Controllers
+{
+    public class ABMPublicacionController : Controller
+    {
+        [SessionUtility]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
