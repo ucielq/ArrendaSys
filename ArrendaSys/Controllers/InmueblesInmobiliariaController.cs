2fd1a7348a31a12885623de666f10d1a399d89f4
msebasoto <msebasoto@gmail.com>
2021-09-13 20:01:23 -0300

    Vistas Seba inmuebles

---
+++ ArrendaSys/Controllers/InmueblesInmobiliariaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArrendaSys.Controllers
+{
+    public class InmueblesInmobiliariaController : Controller
+    {
+        // GET: InmueblesInmobiliaria
+        public ActionResult InmueblesInmobiliariaView()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArrendaSys/Controllers/InmueblesPropietarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArrendaSys.Controllers
+{
+    public class InmueblesPropietarioController : Controller
+    {
+        // GET: InmueblesPropietario
+        public ActionResult InmueblesPropietarioView()
+        {
+            return View();
+        }
+    }
+}
